d9947734e09652f6dcb9da1619b5be42a0e3d960
Kai Janssen <kylie.janssen@ndsu.edu>
2025-04-11 21:20:36 -0500

    initial commit. Added contact-list, add-contact, edit-contact components as well as contact interface and data array.

---
+++ src/app/data.ts
@@ -0,0 +1,16 @@
+import { Contacts } from "./contacts"
+
+export const CONTACTS:any=[
+    {
+        firstName:'John',
+        lastName: 'Doe',
+        phoneNumber: '123-456-7890',
+        email: 'john.doe@example.com'
+    },
+    {
+        firstName:'Jane',
+        lastName: 'Doe',
+        phoneNumber: '111-222-3333',
+        email: 'jane.doe@example.com'
+    }
+]
